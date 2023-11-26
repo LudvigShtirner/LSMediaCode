bbbaeddfee3961d10e0ae52a3c3309cae76267b1
Алексей Филиппов <aleksejfilippov@MacBook-Pro-Aleksej.local>
2023-11-26 15:48:39 +0700

    add scheme

--- Package.swift
+++ Package.swift
@@ -3,27 +3,27 @@
 
 import PackageDescription
 
+private let mediaCode = "LSMediaCode"
+private let mediaCodeTests = "LSMediaCodeTests"
+
 let package = Package(
-    name: "LSMediaCode",
+    name: mediaCode,
     platforms: [.iOS(.v13)],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "LSMediaCode",
-            targets: ["LSMediaCode"]),
+        .library(name: mediaCode,
+                 targets: [mediaCode])
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "LSMediaCode",
+            name: mediaCode,
             dependencies: []),
         .testTarget(
-            name: "LSMediaCodeTests",
-            dependencies: ["LSMediaCode"]),
+            name: mediaCodeTests,
+            dependencies: [
+                .byName(name: mediaCode)
+            ]),
     ]
 )
